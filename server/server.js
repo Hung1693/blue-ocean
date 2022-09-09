2c528f5856c58a712b2370f2683cd42205b9855a
HungNguyen16 <HungNguyen16@github.com>
2022-09-09 12:04:35 -0500

    1st commit express sv setup

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express(); //Initialized express
+const cors = require("cors");
+const { DATABASE_URL, NODE_ENV, PORT } = process.env;
+const { Pool } = require("pg");
+const pool = new Pool({
+  // Format: postgres://user:password@host:5432/database
+  connectionString: DATABASE_URL,
+  ...(NODE_ENV === "production"
+    ? { ssl: { rejectUnauthorized: false } }
+    : {}),
+});
+
+pool.connect((err) => {
+  //Connected Database
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("PostgresSQL Connected");
+  }
+});
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query("select * from users;");
+    res.send(result.rows);
+  } catch (err) {
+    res.send("Error " + err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}.`);
+});
+
